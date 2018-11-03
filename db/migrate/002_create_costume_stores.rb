09cfa217420c586adf64d9225214055a1ee17602
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-03 02:01:12 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,4 @@
-class CreateCostumeStore < ActiveRecord::Migration[5.1]
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_store do |t|
       t.string :name

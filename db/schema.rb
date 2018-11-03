31efe9a8c27a3efa131b21b9a396ed04b362d6ac
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-03 02:07:32 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "costume", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.string "image_url"
     t.integer "size"
-  end
-
-  create_table "costume_store", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
   end
 
 end

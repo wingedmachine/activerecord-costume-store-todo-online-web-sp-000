56d3108818124b828f5ce9ff9622d52019cb476e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-03 02:00:59 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,13 @@
 class CreateHauntedHouse < ActiveRecord::Migration[5.1]
   def change
-
+    create _table :HauntedHouse do |t|
+      t.string :name
+      t.string :location
+      t.string :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
   end
 end

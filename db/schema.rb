ac97bf34f2a4100d9fa2a4cf2dd4f8603f22c16c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-03 02:02:29 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,9 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "costume", force: :cascade do |t|
     t.string "name"
-    t.float "price"
+    t.integer "price"
     t.string "image_url"
-    t.string "size"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "size"
   end
 
   create_table "costume_store", force: :cascade do |t|

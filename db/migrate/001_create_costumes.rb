08220a2effafd076979d06c81fa1aee6e928049c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-03 01:52:30 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -2,7 +2,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costume do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.string :image_url
       t.integer :size
     end
